d6a35a5a72b33c1973132cd17e874bcbb87d825e
daniela <daniela.zolnowska@gmail.com>
2019-06-13 14:45:10 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Angular-ReactiveForms';
+  account_validation_messages = CustomValidators.account_validation_messages;
+  phonePattern = '[- +()0-9]*';
+
+  public registeredUser = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(2)]],
+    surname: ['', [Validators.required, Validators.minLength(2)]],
+    email: ['', [Validators.required, Validators.email]],
+    phone: ['', [Validators.pattern(this.phonePattern)]],
+    password: ['', [Validators.required, Validators.minLength(8), Validators.compose([
+      CustomValidators.patternValidator(/[A-Z]/, { hasCapitalCase: true }),
+      CustomValidators.patternValidator(/[@#$%^&]/, { hasSpecialCharacters: true })
+    ])]],
+    confirmPassword: ['', [Validators.required]],
+    pet: ['', [Validators.required]],
+    address: this.fb.group({
+      city: ['', [Validators.required]],
+      street: ['', [Validators.required]],
+      building: ['', [Validators.required]],
+      flatNo: ['']
+    }),
+    consents: this.fb.group({
+      newsletter: [false, [Validators.requiredTrue]],
+      sms: false
+    })
+  },
+  {
+    validator: CustomValidators.passwordMatchValidator
+});
+
+
+   constructor(private fb: FormBuilder) {
+    this.registeredUser.valueChanges.subscribe(val => {
+      if (this.registeredUser.valid) {
+        console.log(val);
+      }
+    });
+   }
+
+  get name() { return this.registeredUser.get('name'); }
+  get email() { return this.registeredUser.get('email'); }
+
+  onSubmit() {
+    this.cleanForm(this.registeredUser);
+    if (this.registeredUser.valid) {
+      console.table(this.registeredUser);
+    }  else {
+      this.validateAllFormFields(this.registeredUser);
+    }
+  }
+
+  validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    });
+  }
+
+  public cleanForm(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.setValue(formGroup.get(field).value.toString().trim());
+      } else if (control instanceof FormGroup) {
+        this.cleanForm(control);
+      }
+    });
+  }
 }
---
+++ src/app/custom-validators.ts
@@ -0,0 +1,68 @@
+import { ValidationErrors, ValidatorFn, AbstractControl } from '@angular/forms';
+
+export class CustomValidators {
+    public static patternValidator(regex: RegExp, error: ValidationErrors): ValidatorFn {
+        return (control: AbstractControl): { [key: string]: any } => {
+          if (!control.value) {
+            // if control is empty return no error
+            return null;
+          }
+          const valid = regex.test(control.value);
+          return valid ? null : error;
+        };
+      }
+
+    public static passwordMatchValidator(control: AbstractControl) {
+        const password: string = control.get('password').value;
+        const confirmPassword: string = control.get('confirmPassword').value;5
+        if (password !== confirmPassword) {
+          control.get('confirmPassword').setErrors({ NoPassswordMatch: true });
+        }
+      }
+
+    static get account_validation_messages()  {return {
+        name: [
+          { type: 'required', message: 'Imię jest wymagane' },
+          { type: 'minlength', message: 'Imię musi posiadać przynajmniej 2 znaki' },
+          { type: 'maxlength', message: 'Username cannot be more than 25 characters long' },
+          { type: 'pattern', message: 'Your username must contain only numbers and letters' },
+          { type: 'validUsername', message: 'Your username has already been taken' }
+        ],
+        surname: [
+          { type: 'required', message: 'Nazwisko jest wymagane' },
+          { type: 'minlength', message: 'Nazwisko musi posiadać przynajmniej 2 znaki' }
+        ],
+        email: [
+          { type: 'required', message: 'Email jest wymagany' },
+          { type: 'email', message: 'Email jest niepoprawny' }
+        ],
+        phone: [
+          { type: 'pattern', message: 'Telefon może zawierać wyłącznie cyfry, spacje oraz znaki + ( ) -' }
+        ],
+        confirmPassword: [
+          { type: 'required', message: 'Proszę potwierdzić hasło' },
+          { type: 'areEqual', message: 'Password mismatch' }
+        ],
+        password: [
+          { type: 'required', message: 'Hasło jest wymagane' },
+          { type: 'minlength', message: 'Hasło musi składać się z co najmniej 8 znaków' },
+          { type: 'hasCapitalCase' && 'hasSpecialCharacters', message: 'Hasło musi zawierać przynajmniej jedną wielką literę oraz jeden ze znaków: @ # $ % ^ &' }
+        ],
+        pet: [
+          { type: 'required', message: 'Proszę wybrać zwierzę' }
+        ],
+        city: [
+            { type: 'required', message: 'Miasto jest wymagane' }
+          ],
+        street: [
+            { type: 'required', message: 'Ulica jest wymagana' }
+          ],
+        building: [
+            { type: 'required', message: 'Nr budynku jest wymagany' }
+          ],
+          newsletter: [
+            { type: 'requiredTrue', message: 'Zgoda na newsletter jest wymagana' }
+          ],
+        };
+    }
+}
